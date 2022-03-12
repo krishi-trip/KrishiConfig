5fce9cedeb33bc5cf4f94a3a9e94313a326cf28a
krishi-trip <66849513+krishi-trip@users.noreply.github.com>
2022-03-12 10:27:16 -0500

    New Emacs Config

--- Emacs/init.el
+++ Emacs/init.el
@@ -1,74 +1,190 @@
-(add-to-list 'load-path "~/neotree")
-  (require 'neotree)
-  (global-set-key (kbd "C-x C-\\") 'neotree-toggle)
+;; beginning of init.el
+
+(setq gc-cons-threshold 100000000)
 
 (setq inhibit-startup-message t)
 
-(load-theme 'tango-dark)
+(setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
 
-(add-hook 'emacs-startup-hook 'my-startup-fcn)
-(defun my-startup-fcn ()
-  "do fancy things"
-  (let ((my-buffer (get-buffer-create "my-buffer")))
-    (with-current-buffer my-buffer
-      ;; this is what you customize
-      (insert "\n\n\n\n\n\n\n\n
-                                                                                         `..`
-                                                                                       ````+ `.`
-                                                                                   /o:``   :+ ``
-                                                                               .+//dho......y/..`
-                                                                               `sdddddhysso+h` ``
-                                                                                 /ddd+`..` +. .`
-                                                                                -hos+    `.:```
-                                                                              `./dddyo+//osso/:`
-                                                                            `/o++dddddddddddddod-
-                                                                           `// -y+:sdddddsddsy.dy
-                                                                               /o   `..```h+`y+/h+`
-                                                                               .s       `++``o:  ``
-                                                                                       `:- `:-
+(if (display-graphic-p)
+    (progn
+      (scroll-bar-mode -1)        ; Disable visible scrollbar
+      (tool-bar-mode -1)          ; Disable the toolbar
+      (tooltip-mode -1)           ; Disable tooltips
+      (set-fringe-mode 10)        ; Give some breathing room
+      )
+  )
 
 
+(menu-bar-mode -1)            ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+;;(load-theme 'wombat)
 
 
-                                                                  “Design is where science and art break even.”
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(use-package command-log-mode)
+
+(use-package swiper
+  :ensure t)
+
+(use-package counsel
+  :ensure t)
+
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode))
+
+(setq swiper t)
 
 
-"))
-    (switch-to-buffer my-buffer)))
+(use-package all-the-icons
+  :if (display-graphic-p)
+  :ensure t)
 
 
-;; add and configure line numbers and column numbers
-(setq line-number-mode t)
-(setq column-number-mode t)
-(global-linum-mode 1)
-(setq linum-format "%d ")
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
 
-;; set a dedicated directory for backup files
-(setq backup-directory-alist `(("." . "~/.saves")))
+(projectile-mode +1)
 
-;; set a backup policy
-(setq make-backup-files t)
-(setq version-control t)
-(setq kept-new-versions 5)
-(setq delete-old-versions t)
+(use-package dashboard
+  :ensure t
+  :init
+  (progn
+    (setq dashboard-startup-banner 3)
+    (setq dashboard-set-file-icons t)
+    (setq dashboard-set-heading-icons t)
+    (setq dashboard-center-content t)
+    ;; Format: "(icon title help action face prefix suffix)"
+    (setq dashboard-set-navigator t)
+    (setq dashboard-items '((recents  . 5)
+			    (bookmarks . 5)
+			    (projects . 5)))
+    (setq dashboard-modify-heading-icons '((recents . "file-text")
+					   (bookmarks . "book")))
+    (setq dashboard-item-names '(("Recent Files:" . "Recent files:")
+				 ("Bookmarks:" . "Bookmarks:")
+				 ("Projects:" . "Git Projects:")))
+    ;;  (setq dashboard-init-info "\n\n\n\n")
+    ;;  (setq dashboard-set-footer nil)
+    )
+  :config
+  (dashboard-setup-startup-hook))
 
-;; no tab characters in whitespace
-(setq-default indent-tabs-mode nil)
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
 
-;; handle indentation with 4 white spaces
-(setq-default c-default-style "linux"
-              c-basic-offset 4)
-(setq-default tab-width 4)
-(setq indent-line-function 'insert-tab)
+(use-package vterm
+    :ensure t)
 
-;; handle multi-line inline lambda expressions
-(setq c-offsets-alist '((arglist-cont-nonempty . 0)))
+(require 'which-key)
+(which-key-mode)
 
-;; auto remove trailing white space
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(use-package kaolin-themes
+  :config
+  (load-theme 'kaolin-blossom t))
 
-;; highlight lines that exceed some column limit
-(setq-default
- whitespace-line-column 100
- whitespace-style '(face lines))
-(add-hook 'prog-mode-hook #'whitespace-mode)
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(global-set-key (kbd "C-c c") 'dashboard-refresh-buffer)
+(global-set-key (kbd "C-c v") 'vterm)
+(global-set-key (kbd "C-c m") 'magit)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#272822" "#F92672" "#A6E22E" "#E6DB74" "#66D9EF" "#FD5FF0" "#A1EFE4" "#F8F8F2"])
+ '(compilation-message-face 'default)
+ '(custom-safe-themes
+   '("d516f1e3e5504c26b1123caa311476dc66d26d379539d12f9f4ed51f10629df3" "c95813797eb70f520f9245b349ff087600e2bd211a681c7a5602d039c91a6428" "e266d44fa3b75406394b979a3addc9b7f202348099cfde69e74ee6432f781336" "d9a28a009cda74d1d53b1fbd050f31af7a1a105aa2d53738e9aa2515908cac4c" "249e100de137f516d56bcf2e98c1e3f9e1e8a6dce50726c974fa6838fbfcec6b" "06ed754b259cb54c30c658502f843937ff19f8b53597ac28577ec33bb084fa52" "11cc65061e0a5410d6489af42f1d0f0478dbd181a9660f81a692ddc5f948bf34" "3c7a784b90f7abebb213869a21e84da462c26a1fda7e5bd0ffebf6ba12dbd041" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" default))
+ '(fci-rule-color "#3C3D37")
+ '(highlight-changes-colors '("#FD5FF0" "#AE81FF"))
+ '(highlight-tail-colors
+   '(("#3C3D37" . 0)
+     ("#679A01" . 20)
+     ("#4BBEAE" . 30)
+     ("#1DB4D0" . 50)
+     ("#9A8F21" . 60)
+     ("#A75B00" . 70)
+     ("#F309DF" . 85)
+     ("#3C3D37" . 100)))
+ '(magit-diff-use-overlays nil)
+ '(package-selected-packages
+   '(color-theme-sanityinc-tomorrow monokai-theme spacemacs-theme zenburn-theme company projectile rainbow-delimiters which-key dashboard doom-modeline ivy command-log-mode use-package))
+ '(pos-tip-background-color "#FFFACE")
+ '(pos-tip-foreground-color "#272822")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   '((20 . "#F92672")
+     (40 . "#CF4F1F")
+     (60 . "#C26C0F")
+     (80 . "#E6DB74")
+     (100 . "#AB8C00")
+     (120 . "#A18F00")
+     (140 . "#989200")
+     (160 . "#8E9500")
+     (180 . "#A6E22E")
+     (200 . "#729A1E")
+     (220 . "#609C3C")
+     (240 . "#4E9D5B")
+     (260 . "#3C9F79")
+     (280 . "#A1EFE4")
+     (300 . "#299BA6")
+     (320 . "#2896B5")
+     (340 . "#2790C3")
+     (360 . "#66D9EF")))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   '(unspecified "#272822" "#3C3D37" "#F70057" "#F92672" "#86C30D" "#A6E22E" "#BEB244" "#E6DB74" "#40CAE4" "#66D9EF" "#FB35EA" "#FD5FF0" "#74DBCD" "#A1EFE4" "#F8F8F2" "#F8F8F0")))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
